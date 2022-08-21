559d0f080040b56bac6066c80739cb7cd2b8e38b
Zachary Dowell <zdowell97@gmail.com>
2022-08-21 11:06:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState } from 'react'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let [search, setSearch] = useState('')
+	let [message, setMessage] = useState('Search for Music!')
+	let [data, setData] = useState([])
+
+	const API_URL = 'https://itunes.apple.com/search?term='
+
+	useEffect(() => {
+		if(search) {
+			const fetchData = async () => {
+				document.title = `${search} Music`
+				const response = await fetch(API_URL + search)
+				const resData = await response.json()
+				if (resData.results.length > 0) {
+					setData(resData.results)
+				} else {
+					setMessage('Not Found')
+				}
+			}
+			fetchData()
+		}
+	}, [search])
+	
+	const handleSearch = (e, term) => {
+		e.preventDefault()
+		setSearch(term)
+	}
+
+	return (
+		<div>
+			<SearchBar handleSearch = {handleSearch}/>
+			{message}
+			<DataContext.Provider value={data}>
+				<Gallery />
+			</DataContext.Provider>
+		</div>
+  	);
 }
 
 export default App;
