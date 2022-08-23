73581ef5b9022e4b9504b2f0cd617a4c0b5d3ca7
Zachary Dowell <zdowell97@gmail.com>
2022-08-23 18:00:01 -0400

    first commit

---
+++ src/components/ArtistView.js
@@ -0,0 +1,19 @@
+
+import { useParams } from 'react-router-dom'
+import {useState} from 'react'
+
+
+function ArtistView() {
+    const { id } = useParams()
+    const [ artistData, setArtistData ] = useState([])
+
+    return (
+        <div>
+            <h2>The id passed was: {id}</h2>
+            <p>Artist Data Goes Here!</p>
+        </div>
+    )
+}
+
+
+export default ArtistView
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,19 @@
-import { useContext } from 'react'
-import { SearchContext } from '../context/SearchContext'
+import { useState } from 'react'
 
-function SearchBar() {
-    const {term, handleSearch} = useContext(SearchContext)
+function SearchBar(props) {
+    let [searchTerm, setSearchTerm] = useState('')
 
     return (
-        <form>
-            <input ref={term} type="text" placeholder="Search Here" />
-            <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
+        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+
+            <input type="text" placeholder="Enter a search term here" onChange={
+                (e) => setSearchTerm(e.target.value)
+            } />
+
+            <input type="submit" />
+
         </form>
     )
 }
-
 
 export default SearchBar
